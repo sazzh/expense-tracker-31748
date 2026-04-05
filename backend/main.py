9f25f693a6ee8edf42d892b4b18e26b00328d8d2
Sarah <sarahwhite944@gmail.com>
2026-04-06 01:55:39 +1000

    Setup sqlite database sqlite with sqlalchemy using litestar plugins

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
 import datetime
 from enum import Enum
 from typing import Optional, Annotated
 from litestar import Litestar, delete, get, post, put
 from litestar.dto import dto_field
+from litestar.plugins.sqlalchemy import SQLAlchemyPlugin, SQLAlchemyAsyncConfig
 from dataclasses import dataclass
 from sqlalchemy.orm import Mapped, registry, mapped_column
 from sqlalchemy import Integer, String, Date, Enum as SqlEnum
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.exc import IntegrityError
+from contextlib import asynccontextmanager
+from typing import Any
+from collections.abc import AsyncGenerator, Sequence
+from litestar.exceptions import ClientException, NotFoundException
 
 # Models
 class CategoryEnum(Enum):
@@ -29,6 +38,14 @@ class Expense:
     category: Mapped[CategoryEnum] = mapped_column(SqlEnum(CategoryEnum))
     description: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
 
+# Setup database
+async def provide_transaction(db_session: AsyncSession) -> AsyncGenerator[AsyncSession, None]:
+    try:
+        async with db_session.begin():
+            yield db_session
+    except IntegrityError as exc:
+        raise ClientException(status_code=409, detail=str(exc)) from exc
+
 # Routes
 @get('/expenses')
 async def get_expenses() -> list[Expense]:
@@ -50,4 +67,15 @@ async def update_expense(expense_id: int, data: Expense) -> Expense:
 async def delete_expense() -> None:
     ...
 
-app = Litestar([get_expenses, get_expense, create_expense, update_expense, delete_expense])
+db_config = SQLAlchemyAsyncConfig(
+    connection_string="sqlite+aiosqlite:///expensetracker.sqlite",
+    metadata=mapper_registry.metadata,
+    create_all=True,
+    before_send_handler="autocommit",
+)
+
+app = Litestar(
+    [get_expenses, get_expense, create_expense, update_expense, delete_expense],
+    dependencies={"transaction": provide_transaction},
+    plugins=[SQLAlchemyPlugin(db_config)],
+)
