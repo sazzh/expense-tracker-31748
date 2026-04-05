a8782b87206526d097e58a847de5cb54f24e49da
Sarah <sarahwhite944@gmail.com>
2026-04-06 01:18:55 +1000

    Create scaffold of expense routes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import datetime
 from enum import Enum
 from typing import Optional, Annotated
-from litestar import Litestar, get
+from litestar import Litestar, delete, get, post, put
 from litestar.dto import dto_field
 from dataclasses import dataclass
 
@@ -23,9 +23,25 @@ class Expense:
     category: CategoryEnum
     description: Optional[str]
 
+# Routes
+@get('/expenses')
+async def get_expenses() -> list[Expense]:
+    ...
 
-@get('/test')
-async def test() -> str:
-    return "welcome to the backend"
+@get('/expenses/{expense_id:int}')
+async def get_expense(expense_id: int) -> Expense:
+    ...
 
-app = Litestar([test])
+@post('/expenses')
+async def create_expense(data: Expense) -> Expense:
+    ...
+
+@put('/expenses/{expense_id:int}')
+async def update_expense(expense_id: int, data: Expense) -> Expense:
+    ...
+
+@delete('/expenses/{expense_id:int}')
+async def delete_expense() -> None:
+    ...
+
+app = Litestar([get_expenses, get_expense, create_expense, update_expense, delete_expense])
