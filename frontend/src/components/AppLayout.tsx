630e09861c0a31fb97ce4e0f68136a9522a828df
Sarah <sarahwhite944@gmail.com>
2026-05-13 05:21:34 +1000

    Add logout
    shown on all pages except for login and register (also checks for token before displaying).
    simply revokes token from storage and redirects to login page.

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -1,13 +1,17 @@
-import { AppShell, Flex, Group, Text } from '@mantine/core';
+import { AppShell, Button, Flex, Group, Text } from '@mantine/core';
 import type { ReactNode } from 'react';
 import { IconCalendarDollar } from '@tabler/icons-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 type AppLayoutProps = {
   children: ReactNode;
 };
 
 export default function AppShellLayout({ children }: AppLayoutProps) {
+  const location = useLocation();
+  const token = localStorage.getItem("token");
+  const showLogout = token && location.pathname !== "/login" && location.pathname !== "/register";
+
   return (
     <AppShell
       header={{ height: { base: 30, md: 40, lg: 50 } }}
@@ -15,12 +19,20 @@ export default function AppShellLayout({ children }: AppLayoutProps) {
       padding="md" >
     
       <AppShell.Header>
-        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <Group h="100%" px="md" gap={8}>
-            <IconCalendarDollar  />
-            <Text>Expense Tracker</Text>
-          </Group>
-        </Link>
+        <Group h="100%" px="md" justify='space-between'>
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Group h="100%" px="md" gap={8}>
+              <IconCalendarDollar  />
+              <Text>Expense Tracker</Text>
+            </Group>
+          </Link>
+          {showLogout && (
+          <Button variant='transparent' onClick={() => {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+          }}>Logout</Button>
+          )}
+        </Group>
       </AppShell.Header>
 
       <AppShell.Main>
