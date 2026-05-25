68aa451bb750eea7de998364b8d3c999a540aa75
Sarah <sarahwhite944@gmail.com>
2026-05-26 02:33:45 +1000

    Update error handling and styling of user activity

--- frontend/src/pages/UserActivity.page.tsx
+++ frontend/src/pages/UserActivity.page.tsx
@@ -1,41 +1,63 @@
-import { Box, Group, Text } from "@mantine/core";
+import { Alert, Box, Center, Group, Loader, Paper, Text } from "@mantine/core";
 import { useParams } from "react-router";
 import { getUser, getUserExpenses } from "../api/Users";
 import { useEffect, useState } from "react";
 import type { User } from "../types/User";
 import type { Expense } from "../types/Expense";
 import BackButton from "../components/BackButton";
+import { IconAlertCircle, IconCoin } from "@tabler/icons-react";
 
 export function UserActivityPage() {
   const { id } = useParams();
   const [expenses, setExpenses] = useState<Expense[]>([]);
   const [user, setUser] = useState<User>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchUsers = async () => {
+      setLoading(true);
       try {
         const user: User = await getUser(id!);
         const expenses: Expense[] = await getUserExpenses(id!);
         setUser(user);
         setExpenses(expenses);
       } catch (err) {
-        console.log(err);
+        setError("Unable to retrieve user and/or their expenses. Please try again.");
       } finally {
-        console.log("implement loading status");
+        setLoading(false);
       }
     };
     fetchUsers();
   }, [id]);
 
-  if (!user) {
+  if (loading) {
+		return (
+			<Center h={470}>
+				<Loader />
+			</Center>
+		)
+	}
+
+	if (error) {
+		return (
+			<Center>
+				<Alert icon={<IconAlertCircle size={16} />} color="red" maw={500}>
+					{error}
+				</Alert>
+		</Center>
+		)
+	}
+
+	if (!user) {
     return (
-      <Box p="xl">
-        <Text>Loading...</Text>
-      </Box>
+      <>
+      <Alert icon={<IconAlertCircle size={16} />} color="red">
+        User not found
+      </Alert>
+      </>
     );
   }
-
-  // if not expenses then display no expenses / history
 
   return (
     <>
@@ -46,17 +68,23 @@ export function UserActivityPage() {
 
         <Box mt="lg">
           {expenses.map(expense => (
-            <Box key={expense.id} p="xs" mb="sm" style={{ border: "2px solid #eee", borderRadius: 12 }}>
+            <Paper key={expense.id} shadow="xs" radius="md" p="md" mb="sm" withBorder style={{ borderColor: "#e5e5e5" }}>
               <Group justify="space-between" align="center">
-                <div style={{ paddingLeft: '2rem' }}>
-                  <Text fw={500}>{expense.name}</Text>
-                  <Text size="sm" c="dimmed">{expense.description}</Text>
-                  <Text size="sm" c="dimmed">{expense.category}</Text>
-                  <Text size="sm" c="dimmed">{"$" + (expense.amount).toFixed(2)}</Text>
-                </div>
-                <Text size="sm" c="dimmed" pr="md">{ new Intl.DateTimeFormat('en-AU').format(new Date(expense.created_at))}</Text>
+                <Box style={{ display: 'flex', alignItems: 'center', gap: 3 }}>
+                  <IconCoin stroke={1.25} size={50} style={{ marginLeft: '1rem'}} />
+                  <Box pl="xl">
+                    <Text fw={500} size="lg">{expense.name}</Text>
+                    <Text size="sm" c="dimmed">{expense.description}</Text>
+                    <Text size="sm" c="dimmed">{expense.category}</Text>
+                    <Text size="sm" c="dimmed">${expense.amount.toFixed(2)}</Text>
+                  </Box>
+                </Box>
+                <Box pr="sm" ta="right">
+                  <Text size="xs" c="dimmed">Created: { new Intl.DateTimeFormat('en-AU', { dateStyle: "medium", timeStyle: "short" }).format(new Date(expense.created_at))}</Text>
+                  <Text size="xs" c="dimmed">Last updated: { new Intl.DateTimeFormat('en-AU', { dateStyle: "medium", timeStyle: "short" }).format(new Date(expense.updated_at))}</Text>
+                </Box>
               </Group>
-            </Box>
+            </Paper>
           ))}
         </Box>
       </Box>
